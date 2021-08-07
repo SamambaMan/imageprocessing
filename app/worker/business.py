e78d13d8edff85d38f6f64d2340bf60021289018
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2021-08-07 18:48:37 -0300

    docker installation and configuration fixes basic services implementation

--- app/worker/business.py
+++ app/worker/business.py
@@ -1,10 +1,29 @@
-def resize(input):
-    pass
+import cv2
 
 
-def split(input):
-    pass
+def resize(input, task):
+    size = [int(x) for x in task.split('x')]
+    return [
+        cv2.resize(image, size, interpolation = cv2.INTER_AREA)
+        for image in input
+    ]
 
 
-def blur(input):
-    pass
+def split(input, task):
+    def splitsingle(image):
+        width, height = image.shape[:2]
+        hwidth, hheight = int(width/2), int(height/2)
+
+        slice1 = image[0:hheight, 0:]
+        slice2 = image[0:hheight, 0:]
+        slice3 = image[0:hheight, 0:]
+        slice4 = image[0:hheight, 0:]
+
+    return [
+        splitsingle(image)
+        for image in input
+    ]
+
+
+def blur(input, task):
+    print('blurred')
--- app/worker/services.py
+++ app/worker/services.py
@@ -1,3 +1,4 @@
+import cv2
 from celery import Celery
 from collections import OrderedDict
 from decouple import config
@@ -18,6 +19,25 @@ ORDERED_METHOD_DICT = OrderedDict([
 ])
 
 
+def process_item(job_item):
+    # prepare de parametrized job caracteristics
+    filename = job_item.pop('filename')
+    output = job_item.pop('output')
+
+    imagelist = [cv2.imread(f'/var/file_deposit/{filename}')]
+
+    # iterate ordered keys and do what must be done acording to 
+    # user`s demands
+    for key in job_item.keys():
+        ORDERED_METHOD_DICT[key](
+            imagelist,
+            job_item[key]
+        )
+
+
 @app.task
 def postprocess(job_list):
-    pass
+    # this process assumes that the user will 
+    # specify the order of the operations.
+    for job in job_list:
+        process_item(job)
