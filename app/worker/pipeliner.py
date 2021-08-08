5093a23d6cb36a9226d153b3d42f6bc28e116be1
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2021-08-08 16:47:13 -0300

    small code refactor for code to be more testable

---
+++ app/worker/pipeliner.py
@@ -0,0 +1,55 @@
+import cv2
+from collections import OrderedDict
+from .business import (
+    resize,
+    split,
+    blur
+)
+
+
+ORDERED_METHOD_DICT = OrderedDict([
+    ('resize', resize),
+    ('split', split),
+    ('blur', blur),
+])
+
+
+def output_images(imagelist, striped_filename, extension):
+    for i, image in enumerate(imagelist):
+        cv2.imwrite(
+            f'/var/file_deposit/{striped_filename}-{i}.{extension}',
+            image
+        )
+
+
+def extract_filename(job_item):
+    filename = job_item.pop('filename')
+    striped_filename = '.'.join(filename.split('.')[:-1])
+    output = job_item.pop('output')
+    return filename, striped_filename, output
+
+
+def iterate_jobs(imagelist, job_item):
+    for key in job_item.keys():
+        imagelist = ORDERED_METHOD_DICT[key](
+            imagelist,
+            job_item[key]
+        )
+    return imagelist
+
+
+def process_item(job_item):
+    # prepare de parametrized job caracteristics
+    filename, striped_filename, output = extract_filename(job_item)
+
+    imagelist = [cv2.imread(f'/var/file_deposit/{filename}')]
+
+    # iterate ordered keys and do what must be done acording to
+    # user`s demands
+    imagelist = iterate_jobs(imagelist, job_item)
+
+    output_images(
+        imagelist,
+        striped_filename,
+        output
+    )
--- app/worker/services.py
+++ app/worker/services.py
@@ -1,53 +1,10 @@
-import cv2
 from celery import Celery
-from collections import OrderedDict
 from decouple import config
-from .business import (
-    resize,
-    split,
-    blur
-)
+from .pipeliner import process_item
 
 
 REDIS_SERVER = config('REDIS_SERVER', 'redis')
 app = Celery('tasks', broker=f'redis://{REDIS_SERVER}')
-
-ORDERED_METHOD_DICT = OrderedDict([
-    ('resize', resize),
-    ('split', split),
-    ('blur', blur),
-])
-
-
-def output_images(imagelist, striped_filename, extension):
-    for i, image in enumerate(imagelist):
-        cv2.imwrite(
-            f'/var/file_deposit/{striped_filename}-{i}.{extension}',
-            image
-        )
-
-
-def process_item(job_item):
-    # prepare de parametrized job caracteristics
-    filename = job_item.pop('filename')
-    striped_filename = '.'.join(filename.split('.')[:-1])
-    output = job_item.pop('output')
-
-    imagelist = [cv2.imread(f'/var/file_deposit/{filename}')]
-
-    # iterate ordered keys and do what must be done acording to
-    # user`s demands
-    for key in job_item.keys():
-        imagelist = ORDERED_METHOD_DICT[key](
-            imagelist,
-            job_item[key]
-        )
-
-    output_images(
-        imagelist,
-        striped_filename,
-        output
-    )
 
 
 @app.task
