df8c86e11a7927440e6025ea861da3d8f5ab0e1c
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2021-08-09 22:11:23 -0300

    small refactor to allow parallel image processing

--- app/worker/services.py
+++ app/worker/services.py
@@ -17,4 +17,15 @@ def postprocess(job_list, image_filename_list):
         :param image_filename_list: list of images filenames to work on
     """
     for image_filename in image_filename_list:
-        process_item(job_list, image_filename)
+        process_item_async.delay(job_list, image_filename)
+
+
+@app.task
+def process_item_async(job_list, image_filename):
+    """
+        This wrapper allows to execute item processing in parallel.
+        For parallelism control and server load, this method assumes
+        that the concurrency level of the server is the number of cpus
+        available, since this is the Celery default
+    """
+    process_item(job_list, image_filename)
